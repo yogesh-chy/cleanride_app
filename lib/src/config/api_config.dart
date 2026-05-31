469a5e5d470ccd7fc6dc7c49630dc6ccdf0cc396
Yogesh Chaudhary <yogeshchy635@gmail.com>
2026-05-31 23:30:24 +0545

    done

--- lib/src/config/api_config.dart
+++ lib/src/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   static const baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://localhost:8000/api',
+    defaultValue: 'https://cleanride-b.onrender.com/api',
   );
 }
